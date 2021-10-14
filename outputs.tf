daa9ed067138d537661293891c9c50ef18d4492b
RahulMR42 <rahulmasquee@gmail.com>
2021-10-14 10:29:43 +0000

    first trial

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "show-ads" {
   value = data.oci_identity_availability_domains.ads.availability_domains
 }
 
-output "new-compartment"{
+output "new-oci-compartment"{
 value = oci_identity_compartment.compartment-mr.id
 }
